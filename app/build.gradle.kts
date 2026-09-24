916388aebd6a9d96ace5caadb9a083e542b81a25
drfxai <drfxai@gmail.com>
2026-09-24 21:17:37 +0330

    Build signed ARM64 and universal release APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.raytunnel.vpnrx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
@@ -42,6 +42,14 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug { }
+  }
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("arm64-v8a")
+      isUniversalApk = true
+    }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
